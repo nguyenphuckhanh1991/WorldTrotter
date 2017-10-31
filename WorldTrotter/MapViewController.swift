0684c74ff36e3bf787a9914c57613c4bd1a72eeb
nguyen.phuc.khanh <nguyen.phuc.khanh@framgias-mac-mini.local>
2017-10-31 12:45:16 +0700

    Add MapKIt and MapViewController

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by nguyen.phuc.khanh on 10/31/17.
+//  Copyright © 2017 nguyen.phuc.khanh. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
